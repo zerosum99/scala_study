8f112a9c720a530200719c8e5854bb61c2505b96
Moon Yong Joon <myjlms99@gmail.com>
2021-05-14 19:03:13 +0900

    999991911222

--- scala_collections/src/main/scala/Demo.scala
+++ scala_collections/src/main/scala/Demo.scala
@@ -2,4 +2,10 @@ object Demo extends App {
 
   println(" Hello scala collections world ")
 
+  var z:Array[String] = new Array[String](3)
+  z(0) = "Zara" z 1 = "Nuha"
+  println("array 1 " + z)
+  println("array 2 " + z1)
+  var z1 = new Array[String](3)
+
 }
